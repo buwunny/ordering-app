0cbd8f0703578cba833cb56c77fcdff73b2934e4
bunny <rortiz6868@gmail.com>
2024-05-12 21:37:48 -0400

    fixed warnings

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Login from './Login';
-import Requests from './Requests';
-import Form from './Form';
-import Orders from './Orders';
-import Upload from './Upload';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+const Home = React.lazy(() => import('./Home'));
+const Login = React.lazy(() => import('./Login'));
+const Requests = React.lazy(() => import('./Requests'));
+const Form = React.lazy(() => import('./Form'));
+const Orders = React.lazy(() => import('./Orders'));
+const Upload = React.lazy(() => import('./Upload'));
 
 function App() {
     return (
         <Router>
-            <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/requests" element={<Requests />} />
-                <Route path="/form" element={<Form />} />
-                <Route path="/orders" element={<Orders />} />
-                <Route path="/upload" element={<Upload />} />
-            </Routes>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/requests" element={<Requests />} />
+                    <Route path="/form" element={<Form />} />
+                    <Route path="/orders" element={<Orders />} />
+                    <Route path="/upload" element={<Upload />} />
+                </Routes>
+            </Suspense>
         </Router>
     );
 }
